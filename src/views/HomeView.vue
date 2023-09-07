0763c45ad36583b44dd7f7d52760c0792ad15d36
chris <kristina.beidina@gmail.com>
2023-09-07 17:14:00 +0300

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+function selectDir() {
+  console.log('selectDir')
+}
+</script>
+
+<template>
+  <main>
+    <button @click="selectDir">
+      Выбрать папку
+    </button>
+  </main>
+</template>
