defa3da4df0651e09763e1887ebd975de067a17b
chris <kristina.beidina@gmail.com>
2023-09-10 15:48:19 +0300

    minor refactor

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './assets/main.css'
 
-import { createApp, watch } from 'vue'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import idb from './services/idb'
 
 // Check that service workers are supported
 if ('serviceWorker' in navigator) {
